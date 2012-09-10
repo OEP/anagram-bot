5ae34d049cdd3b6a7978e2a4c8adc1457e1157d0
Paul Kilgo <paulkilgo@gmail.com>
2012-09-10 09:08:31 -0400

    loading trie in place

--- src/anagram.py
+++ src/anagram.py
@@ -14,15 +14,29 @@ class Anagram:
     fp = open(wordlist)
 
     for word in fp:
-      self._addWord(word)
+      self._addWord(word.upper().strip())
+
+  def _getRoot(self, char):
+    """Returns a root node representing 'char'."""
+    return self._roots.get(char, None)
+
+  def _setDefault(self, char):
+    """Returns root node for 'char' or create if it does not exist."""
+    return self._roots.setdefault(char, Anagram.Node(char))
 
   def _addWord(self, word):
+    """Adds word to the trie."""
     if len(word) < self._minWordSize:
       return
 
     node = None
     for c in word:
       if node == None:
+        node = self._setDefault(c)
+      else:
+        node = node._setDefault(c)
+
+    node._flagTerminal()
       
 
 
@@ -31,3 +45,18 @@ class Anagram:
     def __init__(self, letter):
       self._letter = letter
       self._nextMap = dict()
+
+    def _flagTerminal(self):
+      self._nextMap[None] = None
+
+    def _get(self, letter):
+      return self._nextMap.get(letter)
+
+    def _setDefault(self, letter):
+      return self._nextMap.setdefault(letter, Anagram.Node(letter))
+
+    def __str__(self):
+      return self._letter
+
+    def __repr__(self):
+      return self.__str__()
