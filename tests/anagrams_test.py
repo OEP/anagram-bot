f1e1b9ebdd95c332a7511f977fb8dd3883f6216a
Paul Kilgo <paulkilgo@gmail.com>
2012-09-10 14:51:04 -0400

    added anagram test for two words

--- tests/anagrams_test.py
+++ tests/anagrams_test.py
@@ -5,17 +5,30 @@ from anagram_bot import anagram
 class AnagramTests(unittest.TestCase):
 
   WORDLIST1 = 'wordlists/test1.txt'
+  WORDLIST2 = 'wordlists/test2.txt'
+  SOLUTION2 = 'wordlists/result2.txt'
 
   def setUp(self):
     self._anagram1 = anagram.Anagram(AnagramTests.WORDLIST1)
+    self._anagram2 = anagram.Anagram(AnagramTests.WORDLIST2)
 
   def test_oneWord(self):
-    words = []
-    with open(AnagramTests.WORDLIST1) as fp:
-      for word in fp:
-        words.append(word)
+    solution = self._getSolution(AnagramTests.WORDLIST1)
+    start = solution[0]
+    out = list(self._anagram1.solve(start, len(solution) + 1))
+    self.assertEqual(sorted(solution),sorted(out))
 
-    start = words[0]
+  def test_twoWords(self):
+    solution = self._getSolution(AnagramTests.SOLUTION2)
+    start = solution[0]
+    out = list(self._anagram2.solve(start, len(solution) + 1))
+    self.assertEqual(sorted(solution), sorted(out))
 
-    for i in self._anagram1.solve(start, len(words)):
-      print i
+  def _getSolution(self, filename):
+    solution = []
+    with open(filename) as fp:
+      for line in fp:
+        line = line.upper()
+        line = line.strip()
+        solution.append(line)
+    return solution
