7cc4bd3e8f0de3beedfdff7b1da4baf934cb5ec2
Paul Kilgo <paulkilgo@gmail.com>
2012-09-13 19:45:54 -0400

    added command line script

---
+++ palindromes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+from wordplay.wordplay import Wordplay
+
+def printUsage(progName):
+  print "Usage: %s [cipher1] [cipher2] ..." % progName
+
+def main():
+  if len(sys.argv) < 2:
+    printUsage(sys.argv[0])
+    sys.exit(1)
+
+  print "Loading words."
+  wp = Wordplay('wordlists/simple.txt')
+  print "Finished."
+
+  for cipher in sys.argv[1:]:
+    print "Computing %s" % cipher
+    count = 0
+    for palindrome in wp.solvePalindrome(cipher):
+      if len(sys.argv) > 2 and count == 0:
+        print "Palindromes of: %s" % cipher
+      count += 1
+      print palindrome
+    if count == 0:
+      print "No palindromes found for %s" % cipher
+
+if __name__ == "__main__":
+  main()
